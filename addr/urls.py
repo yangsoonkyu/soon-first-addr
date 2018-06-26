dfc111eca9848123bb7ac6fc634551cc79577347
Your Name <t_j4331@naver.com>
2018-06-26 10:41:00 +0900

    상세보기 추가

--- addr/urls.py
+++ addr/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.friend_list, name='friend_list'),
+    url(r'^friend/(?P<pk>\d+)/$', views.friend_detail, name='friend_detail'),
 ]
--- addr/views.py
+++ addr/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Friend
 
 # Create your views here.
@@ -7,3 +7,8 @@ from .models import Friend
 def friend_list(request):
     friends = Friend.objects.filter(approved_friend=1)
     return render(request, 'addr/friend_list.html',{'friends': friends})
+
+
+def friend_detail(request, pk):
+    friend = get_object_or_404(Friend, pk=pk)
+    return render(request, 'addr/friend_detail.html', {'friend': friend})
